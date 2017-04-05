7fe238cacfbcee8a3ac326ff2947f1b644db309a
antoniostasi <antonio.stasi@exprivia.it>
2017-04-05 08:03:48 +0000

    Primo Master Push

---
+++ webapp/Component.js
@@ -0,0 +1,58 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent",
+	"sap/ui/Device",
+	"org/fater/app/model/models"
+], function(UIComponent, Device, models) {
+	"use strict";
+
+	return UIComponent.extend("org.fater.app.Component", {
+
+		metadata: {
+			manifest: "json"
+		},
+
+		/**
+		 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+		 * @public
+		 * @override
+		 */
+		init: function() {
+			// call the base component's init function
+			UIComponent.prototype.init.apply(this, arguments);
+
+			// set the device model
+			this.setModel(models.createDeviceModel(), "device");
+			
+			this.getRouter().initialize();
+		},
+		
+		parseUserInfo: function() {
+			var id = null;
+			
+			try {
+				var userShell = sap.ushell.Container.getService("UserInfo").getUser();
+				id = userShell.getId().toUpperCase();
+			} catch ( err ) {}
+			
+			if( id === null || id === undefined ) {
+				try {
+					id = "RIF_COMM_01";
+				} catch ( err ) {}
+			}
+			
+			var oUtilsModel = this.getModel("utils");
+			if( oUtilsModel ) {
+				this.getModel("oUserModel").read("/UserSet('" + id +"')", {
+					async: false,
+					success: function(oUserOData) {
+						oUtilsModel.setProperty("/User",oUserOData);
+					},
+					error: function(error) {
+						
+					}
+				});
+			}
+		}
+	});
+
+});
---
+++ webapp/util/formatter.js
@@ -0,0 +1,28 @@
+sap.ui.define([
+	], function () {
+		"use strict";
+
+		return {
+
+			companyName: function(bukrs, ekorg){
+				if (this.getId()){
+					this.bindElement("oDataModel>/CompanySet('" + bukrs + "')");					
+				}
+			},
+			
+			commercialManager: function(resp){
+				if (resp){
+					try {
+						var oRespArray = this.getView().getModel("oRespModel").getProperty("/RespSet");
+						var User = oRespArray.filter(function(item) { return item.Username === resp; });
+						return User[0].Address.Firstname + " " + User[0].Address.Lastname;	
+					} catch (e) {
+						
+					}
+				}
+				return "";
+			}
+			
+		};
+	}
+);
